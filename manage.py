b8e1bd7d0613cc397c8716c4d73024d4e4ce251a
morreene <morreene@gmail.com>
2016-06-20 20:05:01 +0200

    test2

--- manage.py
+++ manage.py
@@ -1,12 +1,8 @@
 # encoding=utf-8
  
-#from flask.ext.script import Manager
 from flask_script import Manager
-#from application.app import app
-
 from simplebb import app
 from simplebb import models
-
 
 manager = Manager(app)
 app.config['DEBUG'] = True
